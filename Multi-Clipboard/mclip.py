78f629f2ba61355cc0de94e54cf9e4b224b7b7e0
adawatia <122260173+adawatia@users.noreply.github.com>
2025-03-03 12:47:01 +0530

    mclip updated.

---
+++ Multi-Clipboard/mclip.py
@@ -0,0 +1,13 @@
+# mclip.py - A multi-clipboard program.
+TEXT =  {
+    'agree':"""Yes, I agree. That sound fine to me.""",
+    'busy':"""Sorry, can we do this later this week or next week?""",
+    'upsell':"""Would you consider making this monthly donation?"""
+}
+
+import sys, pyperclip
+if len(sys.argv) < 2:
+    print(f"Usage: python mclip.py [keyphrase] - copy phase text")
+    sys.exit()
+
+keyphrase = sys.argv[1]
