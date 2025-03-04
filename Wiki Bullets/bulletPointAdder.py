28a285f92b86c591d6b14acc690b5355ee2cf324
adawatia <122260173+adawatia@users.noreply.github.com>
2025-03-04 19:58:16 +0530

    Code complete.

--- Wiki Bullets/bulletPointAdder.py
+++ Wiki Bullets/bulletPointAdder.py
@@ -1,7 +1,13 @@
 # python3
 # bulletPointAdder.py - - Adds Wikipedia bullet points to the start of each line of text on the clipboard.
 
-import pyperclip
-text = pyperclip.paste()
-# TODO: Seperate lines and add stars
-pyperclip.copy(text)
+import pyperclip #* Module to interact with system copy and paste
+text = pyperclip.paste() #* Get what  is copied in clipboard last time
+lines = text.split('\n') #* Slpit copied lines 
+for i in range(len(lines)): #* To add * in everyline
+    lines[i] = '*' + lines[i]
+    
+text = '\n'.join(lines) #* Join all lines into one
+pyperclip.copy(text) #* send modilied text to clipboard
+
+
